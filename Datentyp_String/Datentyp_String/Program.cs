a0c2b4c36fa2c3b896c86d449eb685b98554dd59
akrlfix <krull_1@gmx.de>
2017-01-14 18:28:12 +0100

    first

---
+++ Datentyp_String/Datentyp_String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.String text = "Text";
+            string text = "Text";
+
+            char zeichen = text[2];
+
+            // immutable -> nicht veränderbar
+            //text[2] = 's';
+
+            string hallo = "Hallo";
+            string welt = "Welt";
+            string halloWelt = hallo + " " + welt;
+
+            string neueZeile = "\n in einer \"neuen\" Zeile \tDies ist ein Backslash \\";
+            string pfad = "C:\\temp";
+            string verbatimString = @"C:\temp";
+        }
+    }
+}
